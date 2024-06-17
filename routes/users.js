5c2af0f84062e00afea1762f8fd3916a3207b150
aditya27t <aditpoetra25@gmail.com>
2024-06-17 18:45:54 +0700

    feat: add users routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../app/controllers/auth.controller');
+const { jwtAuth } = require('../middleware/jwtAuth');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/login', controller.login);
 
 
 module.exports = router;
